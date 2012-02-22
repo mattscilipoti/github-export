f8d85649ec4ebdbdd7fed7a4c8b5a5b2bd261024
Matt Scilipoti <matt@scilipoti.name>
2012-02-22 16:03:49 -0500

    Extract (as enumerable) to GHE::Issues

--- spec/export_issues_spec.rb
+++ spec/export_issues_spec.rb
@@ -5,17 +5,36 @@ def test_fixture_repo_uri(suffix = '')
   URI.join('https://api.github.com/repos/mattscilipoti/github-export-fixture/', suffix)
 end
 
-def issues
-  url = test_fixture_repo_uri('issues')
-  return Nestful.json_get url
-  #issues = RestClient.get(url, {:accept => :json})
-  #JSON.parse(issues)
+module GHE
+  class Issues
+    include Enumerable
+
+    def initialize(repo_uri)
+      @repo_uri = repo_uri
+    end
+
+    def repo_uri(suffix = '')
+      URI.join(@repo_uri, suffix)
+    end
+
+    def each &block
+      issues.each{|member| block.call(member)}
+    end
+
+private
+
+    def issues
+      @issues ||= Nestful.json_get repo_uri('issues')
+      #issues = RestClient.get(url, {:accept => :json})
+      #JSON.parse(issues)
+    end
+  end
 end
 
 describe "GET list of issues" do
   use_vcr_cassette
 
-  subject { issues }
+  subject { GHE::Issues.new(test_fixture_repo_uri) }
 
   it 'should return 1 issue' do
     subject.should have(1).keys
