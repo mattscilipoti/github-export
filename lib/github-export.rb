318906a8b6fcd25f46b6a65d24addc1c12ecba50
Matt Scilipoti <matt@scilipoti.name>
2012-02-22 16:09:44 -0500

    Move GHE::Issues to lib

--- lib/github-export.rb
+++ lib/github-export.rb
@@ -1,7 +1,28 @@
 require "github-export/version"
+require 'nestful'
 
-module Github
-  module Export
-    # Your code goes here...
+module GHE
+  class Issues
+    include Enumerable
+
+    def initialize(repo_uri)
+      @repo_uri = repo_uri
+    end
+
+    def repo_uri(suffix = '')
+      URI.join(@repo_uri, suffix)
+    end
+
+    def each &block
+      issues.each{|member| block.call(member)}
+    end
+
+private
+
+    def issues
+      @issues ||= Nestful.json_get repo_uri('issues')
+      #issues = RestClient.get(url, {:accept => :json})
+      #JSON.parse(issues)
+    end
   end
 end
--- spec/export_issues_spec.rb
+++ spec/export_issues_spec.rb
@@ -1,34 +1,8 @@
 require 'spec_helper'
-require 'nestful'
+require File.expand_path('../lib/github-export', File.dirname(__FILE__))
 
 def test_fixture_repo_uri(suffix = '')
   URI.join('https://api.github.com/repos/mattscilipoti/github-export-fixture/', suffix)
-end
-
-module GHE
-  class Issues
-    include Enumerable
-
-    def initialize(repo_uri)
-      @repo_uri = repo_uri
-    end
-
-    def repo_uri(suffix = '')
-      URI.join(@repo_uri, suffix)
-    end
-
-    def each &block
-      issues.each{|member| block.call(member)}
-    end
-
-private
-
-    def issues
-      @issues ||= Nestful.json_get repo_uri('issues')
-      #issues = RestClient.get(url, {:accept => :json})
-      #JSON.parse(issues)
-    end
-  end
 end
 
 describe "GET list of issues" do
