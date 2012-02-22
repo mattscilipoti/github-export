e015f35e6df11fbf86f21457df7b06d8b612251c
Matt Scilipoti <matt@scilipoti.name>
2012-02-22 13:24:44 -0500

    Setup gem with `bundle gem`

---
+++ lib/github-export.rb
@@ -0,0 +1,7 @@
+require "github-export/version"
+
+module Github
+  module Export
+    # Your code goes here...
+  end
+end
---
+++ lib/github-export/version.rb
@@ -0,0 +1,5 @@
+module Github
+  module Export
+    VERSION = "0.0.1"
+  end
+end
