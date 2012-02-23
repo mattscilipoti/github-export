2ca96c4b2f19dc37913aa35e5aed9e20d87fbd9b
Matt Scilipoti <matt@scilipoti.name>
2012-02-23 16:50:27 -0500

    docs.

---
+++ README.md
@@ -0,0 +1,27 @@
+GitHub Exporter
+===============
+
+A library to help you export your data from GitHub.
+
+Utilizes GitHub's API V3.
+
+Supports:
+* Issues 
+
+Usage
+-----
+
+Export to dir (creates a file per issue):
+
+    your_repo_uri = 'https://api.github.com/repos/user_name/repo_name'
+    GHE::Issues.new(your_repo_uri).to_dir(dir_to_export_to)
+
+
+GHE::Issues.new(base_api_repo_url) acts as an array of hashes.  It is enumerable.
+
+
+Considerations
+--------------
+
+Frustrated by continual use of Hash.fetch('key').  Thinking of creating method for each key.
+
--- lib/github-export.rb
+++ lib/github-export.rb
@@ -6,6 +6,7 @@ log_file = File.expand_path('../log/github-export.log', File.dirname(__FILE__))
 Logging.logger = Logger.new(log_file, 'daily' )
 
 module GHE
+  # Acts as an array of hashes.  It is enumerable.
   class Issues
     include Enumerable
     include Logging
@@ -23,6 +24,7 @@ module GHE
       issues.each{|member| block.call(member)}
     end
 
+    # Finds a single issue by number (same id as in url)
     def find(number)
       # Ensure the middle string is not an "endpoint"
       # see: http://apidock.com/ruby/v1_9_2_180/URI/join/class
@@ -45,6 +47,9 @@ module GHE
       'issues'
     end
 
+    # Exports the list of issues to the specified dir.
+    # Since the returned list does not contain all the attributes for each issue
+    #   Each issue is gathered individually and saved ot its own file.
     def to_dir(base_dir = '.')
       path = File.join(base_dir, suffix)
       FileUtils.mkdir_p(path)
